5460ea2e0b80278cbdbb3b9720b99c45e5bf1d5c
hemalakshmi-07 <hemalakshmidhamodharan@gmail.com>
2026-06-08 22:11:32 +0530

    main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import google.generativeai as genai
 import os
 
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
-model = genai.GenerativeModel('gemini-pro')
+model = genai.GenerativeModel('gemini-1.5-flash')
 
 app = FastAPI()
 
